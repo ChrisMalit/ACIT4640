13b3c7ae76ac435124a9bf8132a7cf3c409a7cd0
ChrisMalit <49224820+ChrisMalit@users.noreply.github.com>
2020-09-27 14:59:24 -0700

    quick update

--- app_setup.sh
+++ app_setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash -x
 
-## copy setup directory
+## Copy Setup Folder
 scp -r ./setup todoapp:/home/admin
 
-## this sets the username of the service account used by the todoapp service
-## SERVICE_ACCOUNT must match the user account in configuration files found in ./setup directory
 USER="root"
 PASSWD="P@ssw0rd"
 
